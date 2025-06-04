3e53adc46d2e12647ca7a9182be1fc8541d632eb
Yaswanth Merugumala <137391445+yaswanthmerugumala@users.noreply.github.com>
2025-06-04 22:26:29 +0530

    db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -8,7 +8,7 @@ const connection = mysql.createConnection({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  port: process.env.DB_PORT || 3305, // Use DB_PORT from the .env file
+  port: process.env.DB_PORT || 3306, // Use DB_PORT from the .env file
 });
 
 connection.connect((err) => {
